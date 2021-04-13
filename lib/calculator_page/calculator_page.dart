039b08f35021c5a8b05b87e4930a1ebbb0391102
lfdel24 <lfdel24@gmail.com>
2021-04-13 14:58:35 -0500

    Create calculator view

--- lib/calculator_page/calculator_page.dart
+++ lib/calculator_page/calculator_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_calculator/widgets/calculattor_view.dart';
+import 'package:flutter_calculator/widgets/keyboard_view.dart';
 
 class CalculatorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 480),
+          child: Column(
+            children: [
+              CalculatorView(),
+              KeyboardView(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/calculattor_view.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator/settings.dart';
+
+final textStyle1 = TextStyle(color: Colors.white, fontSize: 30);
+final textStyle2 = TextStyle(color: Colors.white, fontSize: 20);
+
+class CalculatorView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height / 2.7,
+      color: black,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(child: Container()),
+          _Item(
+            text: "1000",
+          ),
+          _Item(
+            text: "x",
+          ),
+          _Item(
+            text: "1000",
+          ),
+          Divider(color: Colors.green),
+          _Item(
+            result: true,
+            text: "2000",
+          ),
+          SizedBox(height: 8)
+        ],
+      ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  final bool result;
+  final String text;
+
+  const _Item({Key? key, this.result = false, this.text = ""})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(right: 12, top: 8),
+      child: Text(text, style: result ? textStyle1 : textStyle2),
+    );
+  }
+}
